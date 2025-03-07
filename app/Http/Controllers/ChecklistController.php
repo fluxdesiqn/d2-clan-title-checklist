78bbf02eedff6a31ad81c4c9df269c96c16ae692
Connor Shaw <connor@warboxcreative.co.uk>
2025-03-07 17:34:01 +0000

    getting the titles of guardians

--- app/Http/Controllers/ChecklistController.php
+++ app/Http/Controllers/ChecklistController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use App\Services\RaidService;
 
 class ChecklistController extends Controller
@@ -22,6 +23,58 @@ class ChecklistController extends Controller
 
     public function submit(Request $request)
     {
-        dd($request->all());
+        $guardians = [];
+        for ($i = 1; $i <= 6; $i++) {
+            $platform = $request->input("platform{$i}");
+            $guardian = $request->input("guardian{$i}");
+            if ($platform && $guardian) {
+                $guardians[] = [
+                    'platform' => $platform,
+                    'guardian' => $guardian,
+                ];
+            }
+        }
+
+        $titles = $this->getGuardianTitles($guardians);
+
+        dd($titles);
+    }
+
+    private function getGuardianTitles($guardians)
+    {
+        $apiKey = env('BUNGIE_API_KEY');
+        $titles = [];
+
+        foreach ($guardians as $guardian) {
+            $platform = $guardian['platform'];
+            $guardianName = $guardian['guardian'];
+
+            // Make API call to get membership ID
+            $membershipResponse = Http::withHeaders([
+                'X-API-Key' => $apiKey,
+            ])->get("https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/{$platform}/{$guardianName}/");
+
+            if ($membershipResponse->failed() || empty($membershipResponse->json()['Response'])) {
+                continue;
+            }
+
+            $membershipId = $membershipResponse->json()['Response'][0]['membershipId'];
+
+            // Make API call to get profile information
+            $profileResponse = Http::withHeaders([
+                'X-API-Key' => $apiKey,
+            ])->get("https://www.bungie.net/Platform/Destiny2/{$platform}/Profile/{$membershipId}/?components=Profiles");
+
+            if ($profileResponse->failed() || empty($profileResponse->json()['Response'])) {
+                continue;
+            }
+
+            $profileData = $profileResponse->json()['Response']['profile']['data'];
+
+            // Extract title information
+            $titles[$guardianName] = $profileData['titleRecordHashes'] ?? [];
+        }
+
+        return $titles;
     }
 }
